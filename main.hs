7611516a0c38ebe12e459e1fdf6720f93a352c64
Joakim <joakimanderlind@gmail.com>
2019-11-27 14:57:39 +0100

    Added argument handling to interpretor

--- main.hs
+++ main.hs
@@ -1,9 +1,12 @@
+import System.Environment
+
 import Parse
 import Eval
 
 main :: IO()
 main = do
-    file <- readFile "Test.uwu"
+    args <- getArgs
+    file <- readFile $ head args
     let ast = parseFile file
     let res = runExprs ast initState
     evalState res
